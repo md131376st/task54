c3388e2aaa5b207a89f0fc0ed028197935570629
Mona Davari <mona131376st@gmail.com>
2018-09-16 00:16:31 +0430

    Update tables.py

--- task4/tables.py
+++ task4/tables.py
@@ -2,7 +2,7 @@ import django_tables2 as tables
 import pandas as pd
 from page.models import CsvDoc
 
-
+# we don't use the functions
 def func():
     for e in CsvDoc.objects.all():
         items = pd.read_csv("media/"+e.csv_file.name).columns.values
