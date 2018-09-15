f2152df267582577bf570743ea25f32c04dc3279
Mona Davari <mona131376st@gmail.com>
2018-09-16 00:27:12 +0430

    Update models.py

--- page/models.py
+++ page/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+# the file field we save are csv files in database 
 class CsvDoc(models.Model):
     csv_file = models.FileField(upload_to='csv/%Y/%m/%d')
     def __str__(self):
