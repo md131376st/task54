9387ba58a6517bf5a1243f3b8596f846513037a6
Mona Davari <mona131376st@gmail.com>
2018-09-16 00:26:10 +0430

    Update views.py

--- page/views.py
+++ page/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .form import Myform
 
-
+#it cheaks if a file is a csv file by cheaking if it ends with .csv if it is not it shows a error massage else it saves in database and shows a sussess massege
 def model_form_upload(request):
     if request.method == 'POST':
         form = Myform(request.POST, request.FILES)
@@ -13,6 +13,7 @@ def model_form_upload(request):
         if form.is_valid():
             form.save()
             messages.success(request,'You CSV file is know saved')
+            # you need to add this line to show the success massage in template
             success = True
             # messages.success(request, 'You CSV file is know saved')
             return redirect('simple_upload')
